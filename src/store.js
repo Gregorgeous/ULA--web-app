bb81e34d971bf97173d75ef17a46be3cb6721254
Grzegorz Rybak <g.r.fisher.pl@gmail.com>
2019-05-02 14:43:17 +0100

    Creating vuex store #3: created ALL mutations that will be needed troughout the app.

--- src/store.js
+++ src/store.js
@@ -10,7 +10,6 @@ import axios from "axios";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  mutations: {},
   actions: {}
 	state: {
 		recordingTime: {
@@ -26,5 +25,47 @@ export default new Vuex.Store({
 			email: ""
 		},
 		loadedAudioFile: null
+	},
+	mutations: {
+		changeRecordingStateTo(state, boolean) {
+			state.isRecordingOn = boolean;
+		},
+		changeLoadingState(state, boolStatus) {
+			state.loadingState = boolStatus;
+		},
+		setCurrentUser(state, payload) {
+			state.currentUser = payload;
+			state.userLoggedIn = true;
+		},
+		setLoadAudioFile(state, payload) {
+			state.loadedAudioFile = payload;
+		},
+		logout(state) {
+			state.currentUser = {
+				fullName: null,
+				email: null,
+				userTextTranscriptions: {},
+				userSummaryURLs: []
+			};
+			state.userLoggedIn = false;
+		},
+		setOneUserSummary(state, payload) {
+			// IDEA: NOTE: THIS WON'T WORK DUE TO JS' BROKEN OBJECTS REACTIVITY SYSTEM (lack of "getter" and "setter" watchers on objects in JS)!
+			//
+			// state.currentUser.userTextTranscriptions[
+			// 	payload.transcriptionTitle
+			// ].summary = payload.summary;
+			// state.currentUser.userTextTranscriptions[
+			// 	payload.transcriptionTitle
+			// ].keywords = payload.keywords;
+			// IDEA: Instead, you need to create a brand new object below. NOW this will make the computed property on ULARecordingItem correctly reactive to changes.
+			let oldTT =
+				state.currentUser.userTextTranscriptions[payload.transcriptionTitle];
+			state.currentUser.userTextTranscriptions[payload.transcriptionTitle] = {
+				...oldTT,
+				summary: payload.summary,
+				keywords: payload.keywords
+			};
+		}
 	},
 });
