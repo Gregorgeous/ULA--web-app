5af46b885d22468524c920844fccb311cd9de418
Grzegorz Rybak <g.r.fisher.pl@gmail.com>
2019-05-02 13:31:34 +0100

    Redesigned Vue initialisation file (main.js) to initialise Firebase BaaS at the start of the app.

--- src/main.js
+++ src/main.js
@@ -1,13 +1,26 @@
+import "@babel/polyfill";
 import Vue from "vue";
-import App from "./App.vue";
+import "./plugins/vuetify";
+import mainLayout from "./MainLayout.vue";
 import router from "./router";
 import store from "./store";
 import "./registerServiceWorker";
 
 Vue.config.productionTip = false;
 
+// INITIALISE FIREBASE HERE
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+import fbConfig from "@/../firebaseConfig";
+
+// IDEA: this 'if' prevents an occasional "Firebase application already initialized" error
+if (!firebase.apps.length) {
+	firebase.initializeApp(fbConfig);
+}
+
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+	router,
+	store,
+	render: h => h(mainLayout)
 }).$mount("#app");
