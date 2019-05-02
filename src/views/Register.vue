34c94f1c51dde891aab62be2d3221163fe455faf
Grzegorz Rybak <g.r.fisher.pl@gmail.com>
2019-05-02 15:19:22 +0100

    Login & Register features #6: Created a Register view that injects the made Register form component.

---
+++ src/views/Register.vue
@@ -0,0 +1,21 @@
+<template>
+	<v-layout class='mt-1'
+	 column
+	 justify-center
+	 align-center>
+		<v-flex xs12
+		 sm8
+		 md6>
+			<registerForm></registerForm>
+		</v-flex>
+	</v-layout>
+</template>
+
+<script>
+import registerForm from "@/components/registerForm";
+export default {
+  components: {
+    registerForm
+  }
+};
+</script>
