ff436dee58a3d8a27903febfe44e85c5978d4c03
Grzegorz Rybak <g.r.fisher.pl@gmail.com>
2019-05-02 15:17:24 +0100

    Login & Register features #3: Created a component-skeleton for a passwordReset component.

---
+++ src/components/passwordReset.vue
@@ -0,0 +1,14 @@
+<template>
+	<!-- TODO: IMPLEMENT ! -->
+	<small>Forgot the password?
+		<router-link to="/">Click here!
+		</router-link>
+	</small>
+</template>
+
+
+<style lang="scss" scoped>
+small {
+  color: #636363;
+}
+</style>
