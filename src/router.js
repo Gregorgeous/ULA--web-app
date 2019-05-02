ee9cfad46d56504d0b97527383620b6b4cea5e3f
Grzegorz Rybak <g.r.fisher.pl@gmail.com>
2019-05-02 15:22:01 +0100

    Creating Vue router route-guarding feature #2: Created the routes for viewing the new home page and the login and register pages.

--- src/router.js
+++ src/router.js
@@ -8,23 +8,29 @@ import Home from "./views/Home.vue";
 
 Vue.use(Router);
 
-export default new Router({
-  mode: "history",
-  base: process.env.BASE_URL,
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: Home
-    },
-    {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
-    }
-  ]
+let router = new Router({
+	mode: "history",
+	base: process.env.BASE_URL,
+	routes: [
+		{
+			path: "/",
+			name: "home",
+			component: Home,
+			meta: {
+				requiresAuth: true
+			}
+		},
+		{
+			path: "/login",
+			name: "login",
+			component: () =>
+				import(/* webpackChunkName: "login" */ "./views/Login.vue")
+		},
+		{
+			path: "/register",
+			name: "register",
+			component: () =>
+				import(/* webpackChunkName: "Register" */ "./views/Register.vue")
+		}
+	]
 });
