06b8cb3685478ed7303caf6ca53617ecc85502e2
Grzegorz Rybak <g.r.fisher.pl@gmail.com>
2019-05-02 15:18:48 +0100

    Login & Register features #5: Created a Login view that injects the newly created login form.

---
+++ src/views/Login.vue
@@ -0,0 +1,21 @@
+<template>
+	<v-layout class='mt-1'
+	 column
+	 justify-center
+	 align-center>
+		<v-flex xs12
+		 sm8
+		 md6>
+			<loginForm></loginForm>
+		</v-flex>
+	</v-layout>
+</template>
+
+<script>
+import loginForm from "@/components/loginForm";
+export default {
+  components: {
+    loginForm
+  }
+};
+</script>
